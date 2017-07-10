3b2496bff5bc51c0f26b9ddf73dcadb08324b79a
Kfir Karmon <kfirk@microsoft.com>
2017-07-11 01:26:40 +0300

    Fix PR comments

--- RockPaperScissors/GesturesRockPaperScissors.cs
+++ RockPaperScissors/GesturesRockPaperScissors.cs
@@ -39,11 +39,7 @@ namespace Microsoft.Gestures.Samples.RockPaperScissors
 
         public async Task Init()
         {
-            // Step1: Connect to Gesture Detection Service and route StatusChanged event to the UI
-            _gesturesService = GesturesServiceEndpointFactory.Create();
-            _gesturesService.StatusChanged += (oldStatus, newStatus) => GesturesDetectionStatusChanged?.Invoke(oldStatus, newStatus);
-
-            // Step2: Define the Rock-Paper-Scissors gestures
+            // Step1: Define the Rock-Paper-Scissors gestures
             // Create a pose for 'Rock'...
             var rockPose = new HandPose("RockPose", new FingerPose(new AllFingersContext(), FingerFlexion.Folded));
             rockPose.Triggered += (s, arg) => UserStrategyChanged?.Invoke(GameStrategy.Rock); 
@@ -64,6 +60,10 @@ namespace Microsoft.Gestures.Samples.RockPaperScissors
             // Very handy when you need to define a Clique (see https://en.wikipedia.org/wiki/Clique_(graph_theory)#1)
             // as in this case where Rock, Paper and Scissors are all connected to each other...
             var epsilonState = new PassThroughGestureSegment("Epsilon");
+            
+            // ...this pose is an artificial stop pose. Namely, we want to keep the gesture detector in one of the pose states without ending the gesture so we add this
+            // pose as a pose that completes the gesture assuming the user will not perform it frequently. 
+            // As long as the user continues to perform the 'Rock', 'Paper' or 'Scissors' poses we will remain within the gesture's state machine.
             var giveUpPose = new HandPose("GiveUpPose", new PalmPose(new AnyHandContext(), PoseDirection.Forward, PoseDirection.Up),
                                                         new FingerPose(new AllFingersContext(), FingerFlexion.Open));
 
@@ -72,9 +72,13 @@ namespace Microsoft.Gestures.Samples.RockPaperScissors
             _gameGesture.AddSubPath(epsilonState, rockPose, epsilonState);
             _gameGesture.AddSubPath(epsilonState, paperPose, epsilonState);
             _gameGesture.AddSubPath(epsilonState, scissorsPose, epsilonState);
+
+            // In case the user performs a pose that is not one of the game poses the gesture resets and this event will trigger
             _gameGesture.IdleTriggered += (s, arg) => UserStrategyChanged?.Invoke(GameStrategy.None);
-                
-            // Step3: Connect to the gesture detection service and register the gesture
+
+            // Step2: Connect to Gesture Detection Service, route StatusChanged event to the UI and register the gesture
+            _gesturesService = GesturesServiceEndpointFactory.Create();
+            _gesturesService.StatusChanged += (oldStatus, newStatus) => GesturesDetectionStatusChanged?.Invoke(oldStatus, newStatus);
             await _gesturesService.ConnectAsync();
             await _gesturesService.RegisterGesture(_gameGesture);
         }

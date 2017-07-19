029227853a085a76ed02be99323a91da1a6105ea
Kfir Karmon <kfirk@microsoft.com>
2017-07-19 16:58:06 +0300

    Move camera when when pinching

--- Camera3D/MainWindow.xaml.cs
+++ Camera3D/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Gestures;
+using Microsoft.Gestures.Endpoint;
+using Microsoft.Gestures.Skeleton;
+using Microsoft.Gestures.Stock.Gestures;
+using Microsoft.Gestures.Stock.HandPoses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,18 +27,43 @@ namespace Camera3D
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Timer _timer;
         private Random _rand = new Random();
+        private GesturesServiceEndpoint _gesturesService;
+        private Gesture _cameraPinch;
+        private Vector3 _lastPalmLocation = new Vector3(0,0,0);
 
         public MainWindow()
         {
             InitializeComponent();
-            _timer = new Timer(OnTimer, null, 0, 1000);
+            Loaded += WindowLoaded;
+            Closed += (s, args) => _gesturesService.Dispose();
         }
 
-        private void OnTimer(object state)
+        private async void WindowLoaded(object sender, RoutedEventArgs e)
         {
-            Dispatcher.InvokeAsync(() => Camera.Position = Camera.Position + new Vector3D(_rand.Next(2), _rand.Next(2), _rand.Next(2)));
+            var pinchClose = new PinchPose("PinchClosePose", pinchSpread: false);
+            pinchClose.Triggered += (s, args) => _gesturesService.RegisterToSkeleton(OnSekeltonReady);
+            _cameraPinch = new Gesture("CameraPinch", new PinchPose("PinchOpenPose", pinchSpread: true), 
+                                                      pinchClose, 
+                                                      new PinchPose("PinchReleasePose", pinchSpread: true));
+            _cameraPinch.Triggered += (s, arg) => _gesturesService.UnregisterFromSkeleton();
+
+            _gesturesService = GesturesServiceEndpointFactory.Create();
+            await _gesturesService.ConnectAsync();
+            await _gesturesService.RegisterGesture(_cameraPinch);
+        }
+
+        private void OnSekeltonReady(object sender, HandSkeletonsReadyEventArgs e)
+        {
+            var currentPalmLocation = e.DefaultHandSkeleton.PalmPosition;
+            var diff = _lastPalmLocation - currentPalmLocation;
+            _lastPalmLocation = currentPalmLocation;
+
+            // Yoni: do the right logic here for smoothing and moving camera
+            if (diff.X < 10 && diff.Y < 10 && diff.Z < 10)
+            {
+                Dispatcher.InvokeAsync(() => Camera.Position = Camera.Position + new Vector3D(diff.X, diff.Y, diff.Z));
+            }
         }
     }
 }

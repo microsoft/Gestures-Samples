ccf07eaf55a47243a60cd0fbbaa669be0e8d3372
Yoni Smolin <yonis@microsoft.com>
2017-07-20 07:06:09 +0300

    Add moving average

--- Camera3D/MainWindow.xaml.cs
+++ Camera3D/MainWindow.xaml.cs
@@ -27,10 +27,14 @@ namespace Camera3D
     /// </summary>
     public partial class MainWindow : Window
     {
+        const int MovingAverageWindowSize = 5;
+
         private Random _rand = new Random();
         private GesturesServiceEndpoint _gesturesService;
         private Gesture _cameraPinch;
-        private Vector3 _lastPalmLocation = new Vector3(0,0,0);
+
+        private MovingAverage _movingWindow;
+        private Vector3 _lastPalmLocationAverage = new Vector3(0, 0, 0);
 
         public MainWindow()
         {
@@ -41,6 +45,7 @@ namespace Camera3D
 
         private async void WindowLoaded(object sender, RoutedEventArgs e)
         {
+            // define the gesture we will be using to rotate the object
             var pinchClose = new PinchPose("PinchClosePose", pinchSpread: false);
             pinchClose.Triggered += (s, args) => _gesturesService.RegisterToSkeleton(OnSekeltonReady);
             _cameraPinch = new Gesture("CameraPinch", new PinchPose("PinchOpenPose", pinchSpread: true), 
@@ -48,16 +53,21 @@ namespace Camera3D
                                                       new PinchPose("PinchReleasePose", pinchSpread: true));
             _cameraPinch.Triggered += (s, arg) => _gesturesService.UnregisterFromSkeleton();
 
+            // connect to the Gestures Service and register the gesture we've defined
             _gesturesService = GesturesServiceEndpointFactory.Create();
             await _gesturesService.ConnectAsync();
             await _gesturesService.RegisterGesture(_cameraPinch);
+
+            // auxiliary classes to control the camera motion
+            _movingWindow = new MovingAverage(MovingAverageWindowSize);
         }
 
         private void OnSekeltonReady(object sender, HandSkeletonsReadyEventArgs e)
         {
-            var currentPalmLocation = e.DefaultHandSkeleton.PalmPosition;
-            var diff = _lastPalmLocation - currentPalmLocation;
-            _lastPalmLocation = currentPalmLocation;
+            _movingWindow.AddNewSample(e.DefaultHandSkeleton.PalmPosition);
+            var currentPalmLocationAverage = _movingWindow.CurrentAverage;
+            var diff = currentPalmLocationAverage - _lastPalmLocationAverage;
+            _lastPalmLocationAverage = currentPalmLocationAverage;
 
             // Yoni: do the right logic here for smoothing and moving camera
             if (diff.X < 10 && diff.Y < 10 && diff.Z < 10)
